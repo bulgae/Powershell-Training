caba746e8c805fdc906365dcbdbfe041793ea3a8
Bulgae <7daysdreamfornew@gmail.com>
2019-08-15 21:40:13 +1200

    Added basic for Powershell 5

---
+++ cmdlets_basic.ps1
@@ -0,0 +1,31 @@
+﻿#####Windows native
+#notepad
+#ipconfig
+#ping
+#calc
+#2+4-1
+
+#set-location C:\
+#get-childitem
+
+#####command
+#dir
+ls
+clear
+
+#####alias
+get-alias cls
+get-alias dir
+
+#### Update discover command list and help
+update-help -force
+get-help
+help #Same as get-help
+man  #Unix version of get-help
+get-help *process*
+get-help Get-Process
+get-help *ipaddress
+get-help New-NetIPAddress
+help *firewall*
+get-help get-*service*
+get-help about_*
