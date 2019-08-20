d2fda38eb31c4995254156ad52d7f23bd9d40462
Bulgae <7daysdreamfornew@gmail.com>
2019-08-20 21:48:00 +1200

    Added modules and snapin

--- cmdlets_basic.ps1
+++ cmdlets_basic.ps1
@@ -29,3 +29,34 @@ get-help New-NetIPAddress
 help *firewall*
 get-help get-*service*
 get-help about_*
+
+## get help more detail
+get-help Get-Process -Detailed
+get-help Get-Help -Full
+
+## Get all service
+get-service
+get-help Get-Service -Detailed
+get-help get-service -ShowWindow # Can search easier
+get-help Get-Service -online # Access latest version if older version of powershell
+##### Syntax in powershell
+#Get-Fake -parm Arg -param -param arg, arg
+get-service -name bits
+get-service -name bits,bfe
+get-service -DisplayName "app*"
+
+Get-Alias -Definition get-service
+Get-Alias -Name gsv
+ps -c dc
+Get-Alias ps
+Get-Process -ComputerName
+## Extend powershell capability with snapin
+get-help *snapin*
+Get-PSSnapin -Registered
+Add-PSSnapin -name *exch*
+## Work with Modules
+get-help *module*
+Get-Module 
+Get-Module -ListAvailable # Get the list of what it is available
+Import-Module -name ActiveDirectory #V2 of Powershell
+Get-Command -Module act
